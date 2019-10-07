6bfe7f3c302442ba721577ecb3a61db75828bd4f
Rasheed Rahman <ra1da35ma@gmail.com>
2019-10-08 00:18:37 +0100

    add env to configuration

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,41 @@
 import { sync } from 'glob';
 import { union } from 'lodash';
+import * as dotenv from 'dotenv';
 
-export default class Config {
-  public static port: number = 3000;
-  public static routes: string = './dist/routes/**/*.js';
-  public static models: string = './dist/models/**/*.js';
+class Config {
+  public port: number = 3000;
+  public routes: string = './dist/routes/**/*.js';
+  public models: string = './dist/models/**/*.js';
 
+  public accessKeyId: string;
+  public secretAccessKey: string;
 
-  public static globFiles(location: string): string[] {
+  public globFiles(location: string): string[] {
     return union([], sync(location));
   }
+
+
+  constructor() {
+    dotenv.config();
+    let path;
+
+    switch (process.env.NODE_ENV) {
+      case 'test':
+        path = `${ __dirname }/../../.env.test`;
+        break;
+      case 'production':
+        path = `${ __dirname }/../../.env.production`;
+        break;
+      default:
+        path = `${ __dirname }/../../.env.development`;
+    }
+
+    dotenv.config({ path: path });
+
+    this.accessKeyId = process.env.accessKeyId;
+    this.secretAccessKey = process.env.secretAccessKey;
+
+  }
 }
+
+export default new Config();
